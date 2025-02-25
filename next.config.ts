c5991ddfc744ebfdedd12465365388af2ebcf958
Mohamad Rabbani <143613695+MRabbani007@users.noreply.github.com>
2025-02-26 01:06:01 +0500

    image config
    added next image config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "honest-surprise-ee6d3b6dd4.strapiapp.com", // Replace with your image source domain
+      },
+    ],
+    domains: ["strapiapp.com"], // Allow external images from these domains
+  },
 };
 
 export default nextConfig;
