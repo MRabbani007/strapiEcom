afe587acecb39f6df9a29f60c67fbfd40b0e1390
Mohamad Rabbani <143613695+MRabbani007@users.noreply.github.com>
2025-02-26 01:36:56 +0500

    bug fix

--- src/features/navigation/CatalogueMenuContainer.tsx
+++ src/features/navigation/CatalogueMenuContainer.tsx
@@ -1,5 +1,4 @@
 import { getCatalogueMenuData } from "@/data/loaders";
-import React from "react";
 import CatalogueMenu from "./CatalogueMenu";
 
 export default async function CatalogueMenuContainer() {
--- src/features/navigation/Navbar.tsx
+++ src/features/navigation/Navbar.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import React from "react";
 import CatalogueMenuContainer from "./CatalogueMenuContainer";
 import {
   BadgePercent,
